019c8a2d35df06521ec430d7eeb608af3d103790
Paulo Henrique <ph55@hotmail.com>
2023-06-26 13:34:11 -0300

    changing import locale

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,20 +1,36 @@
 import './form-style.css'
+import { useState } from 'react';
+import Card from '../Card/Card.js'
 
 function Form(){
+
+    const [cardNumber, setCardNumber] = useState("***************************");
+    const [cardHolder, setCardHolder] = useState("*****************");
+    const [cardMm, setCardMm] = useState("**")
+    const [cardYy, setCardYy] = useState("****")
+
     return(
-        <div className='form-box'>
-            <form>
-                <div className='infoUser-box'>
-                    <input type="number" name="cardNumber" id="cardNumber" placeholder='Card Number' />
-                    <input type="text" name="cardHolder" id="cardHolder" placeholder='Card Holder' />
-                </div>
-                <div className='date-box'>
-                    <input type="number" name="expMonth" id="expMonth" placeholder='Expiration mm' />
-                    <input type="number" name="expYaer" id="expYaer" placeholder='Expiration yy' />
-                    <input type="number" name="cvv" id="cvv" placeholder='CVV' />
-                </div>
-                <button type='submit'>Submit</button>
-            </form>
+        <div>
+            <Card
+            cardNumber={cardNumber}
+            cardHolder={cardHolder}
+            cardMm={cardMm}
+            cardYy={cardYy}
+            />
+            <div className='form-box'>
+                <form>
+                    <div className='infoUser-box'>
+                        <input type="text" name="cardNumber" id="cardNumber" maxLength="26" placeholder='Card Number' onChange={(e)=> setCardNumber(e.target.value)}/>
+                        <input type="text" name="cardHolder" id="cardHolder" maxLength="15" placeholder='Card Holder' onChange={(e)=> setCardHolder(e.target.value)}/>
+                    </div>
+                    <div className='date-box'>
+                        <input type="number" name="expMonth" id="expMonth" placeholder='mm' onChange={(e)=> setCardMm(e.target.value)}/>
+                        <input type="number" name="expYaer" id="expYaer" placeholder='yy' onChange={(e)=> setCardYy(e.target.value)}/>
+                        <input type="number" name="cvv" id="cvv" placeholder='CVV' />
+                    </div>
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
