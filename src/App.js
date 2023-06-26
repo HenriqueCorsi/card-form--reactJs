d407507fdfc1a3066f1d14d001535be3f2ac7446
Paulo Henrique <ph55@hotmail.com>
2023-06-26 13:34:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Form from './components/Form/Form';
-import Card from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      <Card/>
       <Form/>
     </div>
   );
