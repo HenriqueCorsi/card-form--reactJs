6d898618e017520809ca975b68945dc856522568
Paulo Henrique <ph55@hotmail.com>
2023-06-26 12:02:53 -0300

    add card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,38 @@
+import './style-card.css'
+import Chip from "../../chip.png"
+
+function Card(){
+    return(
+        <div className='container'>
+            <div className='card'>
+                {/*FRONT*/}
+                <div className='front'>
+                    <div className='first-field'>
+                        <img src={Chip} alt="icon chip" />
+                        <h3>Visa</h3>
+                    </div>
+                    <div className='card-number'>
+                        <p id='input-number'>**************************</p>
+                    </div>
+                    <div className='card-holder'>
+                        <h2>Card Holder</h2>
+                    </div>
+                    <div className='user-input'>
+                        <p id='user-name'>**********************</p>
+                        <div className='date-box-input'>
+                            <p id='month'>06</p>
+                            <p id='year'>2026</p>
+                        </div>
+                    </div>
+                </div>
+                {/*BACK*/}
+                <div className='back'>
+                    back
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
+
