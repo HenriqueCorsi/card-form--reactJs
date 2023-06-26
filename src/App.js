1fbbc891dd95936276b6b96332777578cc0969cd
Paulo Henrique <ph55@hotmail.com>
2023-06-26 12:03:19 -0300

    import componet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Form from './components/Form/Form';
+import Card from './components/Card/Card';
 
 function App() {
   return (
     <div className="App">
+      <Card/>
       <Form/>
     </div>
   );
